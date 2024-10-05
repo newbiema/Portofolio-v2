f1dc27c1d9dfc5c4be32c1301031b234043cbd4e
Evan Jamaq <xevan19@gmail.com>
2024-10-05 20:57:03 +0700

    firt commit

---
+++ src/utils/constant/LogoLinks.ts
@@ -0,0 +1,49 @@
+import {
+  Discord,
+  Github,
+  Instagram,
+  LinkedIn,
+  Mail,
+  Telegram,
+  Twitter,
+} from '@/components/common/Icons'
+
+export const logoLinks = [
+  {
+    name: 'Github',
+    href: 'https://github.com/newbiema',
+    icon: Github,
+  },
+  {
+    name: 'Twitter',
+    href: '',
+    icon: Twitter,
+    content: '',
+  },
+  {
+    name: 'Instagram',
+    href: 'https://www.instagram.com/n4ve.666/',
+    icon: Instagram,
+    content: 'n4ve.666',
+  },
+  {
+    name: 'LinkedIn',
+    href: 'https://www.linkedin.com/in/evan-jamaq-9b7159283/',
+    icon: LinkedIn,
+  },
+  {
+    name: 'Gmail',
+    href: '',
+    icon: Mail,
+  },
+  {
+    name: 'Discord',
+    href: '',
+    icon: Discord,
+  },
+  {
+    name: 'Telegram',
+    href: '',
+    icon: Telegram,
+  },
+]
---
+++ src/utils/constant/TechIcons.ts
@@ -0,0 +1,79 @@
+import {
+  BiLogoTypescript,
+  BiLogoJavascript,
+  BiLogoGoLang,
+  BiLogoReact,
+  BiLogoNodejs,
+  BiLogoPostgresql,
+} from 'react-icons/bi'
+import { FaBootstrap, FaJava } from 'react-icons/fa'
+import {
+  SiPhp,
+  SiNextdotjs,
+  SiLaravel,
+  SiExpress,
+  SiAdonisjs,
+  SiTailwindcss,
+  SiJest,
+  SiMongodb,
+  SiMysql,
+  SiFramer,
+  SiJetpackcompose,
+  SiPython,
+} from 'react-icons/si'
+
+import { TbBrandKotlin } from 'react-icons/tb'
+
+export const techIcons = [
+  {
+    name: 'JavaScript',
+    icons: BiLogoJavascript,
+  },
+
+  {
+    name: 'TypeScript',
+    icons: BiLogoTypescript,
+  },
+  {
+    name: 'Python',
+    icons: SiPython,
+  },
+
+  {
+    name: 'Java',
+    icons: FaJava,
+  },
+
+  {
+    name: 'Bootstrap',
+    icons: FaBootstrap,
+  },
+
+  {
+    name: 'React',
+    icons: BiLogoReact,
+  },
+  {
+    name: 'NextJS',
+    icons: SiNextdotjs,
+  },
+
+
+  {
+    name: 'NodeJS',
+    icons: BiLogoNodejs,
+  },
+  {
+    name: 'Tailwinds',
+    icons: SiTailwindcss,
+  },
+
+  {
+    name: 'MongoDB',
+    icons: SiMongodb,
+  },
+  {
+    name: 'MySQL',
+    icons: SiMysql,
+  },
+]

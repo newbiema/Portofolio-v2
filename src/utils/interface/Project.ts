e2230770f843e0cd0e1cd0ce1f4f2ca7fd54877d
Evan Jamaq <xevan19@gmail.com>
2024-10-06 18:36:22 +0700

    tambahin projek di page my projek

--- src/utils/interface/Project.ts
+++ src/utils/interface/Project.ts
@@ -10,7 +10,7 @@ export interface IProject {
 export const project: IProject[] = [
   {
     title: 'Cuy Joki',
-    image: '1c_0e2VebS3AdVFsCfBbBPzPOE6tMd7OX',
+    image: '1ay0SfZvbXcOUdaXRBCFOW8qFIMfOevk4',
     type: 'Education',
     deskripsi:
       'Cuy Joki is a task-jockey website that provides a service where individuals can hire others to complete their assignments, including writing tasks, papers, theses, and more.',
@@ -28,12 +28,13 @@ export const project: IProject[] = [
     repo: 'https://github.com/newbiema/desakembangkuning',
   },
   {
-    title: 'Ayu Songket',
-    image: '12VdXiS72Ef-azqDtRKxv8eyawlyi3BLn',
+    title: 'Sora Top Up',
+    image: '1c_0e2VebS3AdVFsCfBbBPzPOE6tMd7OX',
     type: 'E-Commerce',
     deskripsi:
       'Ayu Songket is a website for purchasing songket fabrics from North Tetebatu Village, East Lombok.',
-    repo: 'https://widyagama.ac.id/',
+    demo : 'https://sora-top-up.vercel.app/',
+    repo: 'https://github.com/newbiema/Sora-Top-Up/',
   },
   {
     title: 'Ayu Songket',
@@ -63,59 +64,30 @@ export const project: IProject[] = [
     repo: 'https://github.com/newbiema/XII-1-IPS-Smansik',
   },
   {
-    title: 'Ayu Songket',
-    image: '12VdXiS72Ef-azqDtRKxv8eyawlyi3BLn',
-    type: 'E-Commerce',
+    title: '12 IPS 1 v-2',
+    image: '15by-eb5gO_XO2OTfYtKJRGCmhK8x0z0r',
+    type: 'School',
     deskripsi:
       'Ayu Songket is a website for purchasing songket fabrics from North Tetebatu Village, East Lombok.',
-    repo: 'https://widyagama.ac.id/',
+    demo : 'https://xii-ips-1-v2.vercel.app/',
+    repo: 'https://github.com/newbiema/XII-IPS-1-v2',
   },
   {
-    title: 'Ayu Songket',
-    image: '12VdXiS72Ef-azqDtRKxv8eyawlyi3BLn',
+    title: 'Sukma Tailors',
+    image: '1IxhD8UCkqtYtTKwXL0OVrl4dNi2OwuEC',
     type: 'E-Commerce',
     deskripsi:
       'Ayu Songket is a website for purchasing songket fabrics from North Tetebatu Village, East Lombok.',
-    repo: 'https://widyagama.ac.id/',
+    demo :'https://sukma-tailor.vercel.app/',
+    repo: 'https://github.com/newbiema/Sukma-Tailor',
   },
   {
-    title: 'Ayu Songket',
-    image: '12VdXiS72Ef-azqDtRKxv8eyawlyi3BLn',
+    title: 'Landing Hero',
+    image: '1q0tfrZVvirZh1zIxPYP-Eaa3kIX6_Jum',
     type: 'E-Commerce',
     deskripsi:
       'Ayu Songket is a website for purchasing songket fabrics from North Tetebatu Village, East Lombok.',
-    repo: 'https://widyagama.ac.id/',
-  },
-  {
-    title: 'Ayu Songket',
-    image: '12VdXiS72Ef-azqDtRKxv8eyawlyi3BLn',
-    type: 'E-Commerce',
-    deskripsi:
-      'Ayu Songket is a website for purchasing songket fabrics from North Tetebatu Village, East Lombok.',
-    repo: 'https://widyagama.ac.id/',
-  },
-  {
-    title: 'Ayu Songket',
-    image: '12VdXiS72Ef-azqDtRKxv8eyawlyi3BLn',
-    type: 'E-Commerce',
-    deskripsi:
-      'Ayu Songket is a website for purchasing songket fabrics from North Tetebatu Village, East Lombok.',
-    repo: 'https://widyagama.ac.id/',
-  },
-  {
-    title: 'Ayu Songket',
-    image: '12VdXiS72Ef-azqDtRKxv8eyawlyi3BLn',
-    type: 'E-Commerce',
-    deskripsi:
-      'Ayu Songket is a website for purchasing songket fabrics from North Tetebatu Village, East Lombok.',
-    repo: 'https://widyagama.ac.id/',
-  },
-  {
-    title: 'Ayu Songket',
-    image: '12VdXiS72Ef-azqDtRKxv8eyawlyi3BLn',
-    type: 'E-Commerce',
-    deskripsi:
-      'Ayu Songket is a website for purchasing songket fabrics from North Tetebatu Village, East Lombok.',
-    repo: 'https://widyagama.ac.id/',
+    demo: 'https://landing-hero-peach.vercel.app/',
+    repo: 'https://github.com/newbiema/Landing-Hero',
   },
 ];
